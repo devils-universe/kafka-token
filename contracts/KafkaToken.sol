e55ea511e3ad53830d2857d76caeadf52044bf2f
devils-universe <jaba_juba420@mail.ru>
2025-06-10 12:20:24 +0700

    Update KafkaToken.sol

--- contracts/KafkaToken.sol
+++ contracts/KafkaToken.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
+
 /**
  * @title Kafka Token ($KAFKA)
- * @dev BEP-20 compatible token for the Devil's Universe project.
+ * BEP-20 compatible token for the Devil's Universe project.
  * Website: https://devilsuniverse.com
  * Twitter: https://twitter.com/devils_kafka
  * Telegram: https://t.me/DevilsUniversecom
