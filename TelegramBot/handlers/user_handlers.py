acbbdfb05e56b45dcc450cb44d1146c646588ff8
devils-universe <jaba_juba420@mail.ru>
2025-07-27 21:57:16 +0700

    Update user_handlers.py

--- TelegramBot/handlers/user_handlers.py
+++ TelegramBot/handlers/user_handlers.py
@@ -1,1 +1,13 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+class StickerOrder(StatesGroup):
+    waiting_for_tx = State()
+    waiting_for_username = State()
+    waiting_for_purpose = State()
+
+def register_user_handlers(dp: Dispatcher):
+    @dp.message_handler(commands=["start"])
+    async def start_command(message: types.Message):
+        await message.answer("Привет! Это KafkaBot 🐿")
 
