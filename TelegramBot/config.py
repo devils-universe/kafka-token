9d935d4c654c34de8bb2cdf554dec7ad88e101a1
devils-universe <jaba_juba420@mail.ru>
2025-07-27 16:35:36 +0700

    Delete TelegramBot directory

--- TelegramBot/config.py
+++
@@ -1,6 +0,0 @@
-import os
-# Main bot token
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# Chat ID where sticker orders will be logged
-LOG_CHAT_ID = os.getenv("LOG_CHAT_ID")
--- TelegramBot/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user_handlers import register_user
-from .admin_handlers import register_admin
-
-def register_handlers(dp):
-    register_user(dp)
-    register_admin(dp)
