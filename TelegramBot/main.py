8987aec4dd453bf45af9af20cf29b43af2523a70
devils-universe <jaba_juba420@mail.ru>
2025-07-27 21:55:15 +0700

    Update main.py

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -1,1 +1,17 @@
+from aiogram import Bot, Dispatcher
+from aiogram.utils import executor
+import config
+from handlers.user_handlers import register_user_handlers
 
+def main():
+    if not config.BOT_TOKEN:
+        raise RuntimeError("BOT_TOKEN is not set")
+
+    bot = Bot(token=config.BOT_TOKEN)
+    dp = Dispatcher(bot)
+
+    register_user_handlers(dp)
+    executor.start_polling(dp)
+
+if __name__ == "__main__":
+    main()
