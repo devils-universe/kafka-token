4687a2a40b3e9ff23942e10397dc25edca967433
devils-universe <jaba_juba420@mail.ru>
2025-07-28 22:15:50 +0700

    Update main.py

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -2,7 +2,7 @@ from bot import bot
 import handlers.user_handlers
 import handlers.buy
 import handlers.tasks
-import handlers.shop
+import handlers.shoop
 import handlers.airdrop
 import handlers.leaderboard
 import handlers.stickers
