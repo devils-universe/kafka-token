50a3f4d521eb2f09e6d8aed2e737852a02b53714
devils-universe <jaba_juba420@mail.ru>
2025-07-28 18:53:43 +0700

    Update shoop.py

--- TelegramBot/handlers/shoop.py
+++ TelegramBot/handlers/shoop.py
@@ -1,5 +1,7 @@
 from bot import bot
 from telebot import types
+from handlers.user_handlers import main_menu
+
 
 @bot.message_handler(func=lambda msg: msg.text == "🛒 Shoop")
 def handle_shoop(message):
@@ -13,8 +15,10 @@ def handle_shoop(message):
         reply_markup=markup
     )
 
+
 @bot.callback_query_handler(func=lambda call: call.data == "open_sticker_store")
 def handle_sticker_store(call):
+    """Показывает стикеры и инструкции по получению."""
     sticker_ids = [
         "CAACAgIAAxkBAAIBuWiHKOetWo-SdCruW2yorH8Wi15nAAI8ewACL4IoSIc0a3D7YkOpNgQ",
         "CAACAgIAAxkBAAIBuGiHKNz3c9yTLXQ7lLYcBm7IkvZdAAILfQACTjAgSKxOMQABd531PTYE",
@@ -23,13 +27,16 @@ def handle_sticker_store(call):
     for sticker_id in sticker_ids:
         bot.send_sticker(call.message.chat.id, sticker_id)
 
-    bot.send_message(
-        call.message.chat.id,
-        "🎁 To receive the full Kafka sticker pack:\n"
-        "1. Send *42 $KAFKA* to the wallet:\n"
+    msg = (
+        "🎁 *To receive the full Kafka sticker pack:*\n"
+        "1. Send *42 $KAFKA* to wallet:\n"
         "`0xef43a15a02345553702c2ef7daa1883e86792f6c`\n"
-        "2. In the transaction comment, write: *stickers*\n"
-        "3. Then DM me the TX hash 🧾",
-        parse_mode="Markdown"
+        "2. In the transaction comment, write: `stickers`\n"
+        "3. Press the button below to send your TX hash 🧾"
     )
+
+    button = types.InlineKeyboardMarkup()
+    button.add(types.InlineKeyboardButton("🧾 Send TX hash", switch_inline_query=""))
+
+    bot.send_message(call.message.chat.id, msg, parse_mode="Markdown", reply_markup=button)
     bot.answer_callback_query(call.id)
