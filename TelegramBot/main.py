ea7b677c927985a43ab7105d265d6b16ea6be6f3
devils-universe <jaba_juba420@mail.ru>
2025-07-28 17:19:40 +0700

    Update main.py

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -1,11 +1,7 @@
 from bot import bot
-
-# Импорт всех хендлеров (очень важно, чтобы они зарегистрировали свои обработчики)
 import handlers.user_handlers
-import handlers.base
 import handlers.buy
 import handlers.tasks
-import handlers.shoop
 
 if __name__ == "__main__":
     bot.remove_webhook()
