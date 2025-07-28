2518009746af4f520266508cf381d0eb586997a3
devils-universe <jaba_juba420@mail.ru>
2025-07-28 22:13:39 +0700

    Update main.py

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -2,7 +2,10 @@ from bot import bot
 import handlers.user_handlers
 import handlers.buy
 import handlers.tasks
+import handlers.shop
+import handlers.airdrop
+import handlers.leaderboard
+import handlers.stickers
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.infinity_polling(skip_pending=True)
+    bot.polling(none_stop=True)
