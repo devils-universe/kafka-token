13c848bfeda3ccfea41ceb10401865eb3e94d788
devils-universe <jaba_juba420@mail.ru>
2025-07-28 23:44:23 +0700

    Update airdrop.py

--- TelegramBot/handlers/airdrop.py
+++ TelegramBot/handlers/airdrop.py
@@ -2,4 +2,4 @@ from bot import bot
 
 @bot.message_handler(func=lambda msg: msg.text == "🎁 Airdrop")
 def handle_airdrop(message):
-    bot.send_message(message.chat.id, "🎁 Join the airdrop now!")
+    bot.send_message(message.chat.id, "🎁 Coming soon!")
