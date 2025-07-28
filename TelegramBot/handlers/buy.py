fafa221f8825c3414019cfaf9df7f61f9812b65b
devils-universe <jaba_juba420@mail.ru>
2025-07-28 17:45:28 +0700

    Update buy.py

--- TelegramBot/handlers/buy.py
+++ TelegramBot/handlers/buy.py
@@ -6,7 +6,7 @@ from handlers.shoop import handle_shoop
 @bot.message_handler(func=lambda msg: msg.text == "🪙 Buy $KAFKA")
 def handle_buy(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("🔗 Buy $KAFKA", url="..."))
+    markup.add(types.InlineKeyboardButton("🔗 Buy $KAFKA", url="https://web3.okx.com/ru/token/bsc/0x0023caf04b4fac8b894fc7fa49d38ddc4606a816"))
     markup.add(
         types.InlineKeyboardButton("Earn / Get Kafka", callback_data="to_tasks"),
         types.InlineKeyboardButton("Create (Spend) Kafka", callback_data="to_shoop")
