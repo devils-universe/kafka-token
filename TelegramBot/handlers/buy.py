9385cff858c2d406e372a6a2ced542f145456a4c
devils-universe <jaba_juba420@mail.ru>
2025-07-28 16:08:21 +0700

    Update buy.py

--- TelegramBot/handlers/buy.py
+++ TelegramBot/handlers/buy.py
@@ -1,29 +1,22 @@
-from telebot import types
 from bot import bot
+from telebot import types
 from handlers.tasks import handle_tasks
 from handlers.shoop import handle_shoop
-
 
 @bot.message_handler(func=lambda msg: msg.text == "🪙 Buy $KAFKA")
 def handle_buy(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("🔗 Buy $KAFKA", url="https://web3.okx.com/ul/1KaUamm"))
+    markup.add(types.InlineKeyboardButton("🔗 Buy $KAFKA", url="..."))
     markup.add(
-        types.InlineKeyboardButton("🪙 Earn / Mine", callback_data="to_tasks"),
-        types.InlineKeyboardButton("🎨 Spend / Create", callback_data="to_shoop")
+      types.InlineKeyboardButton("Earn / Get Kafka", callback_data="to_tasks"),
+      types.InlineKeyboardButton("Create (Spend) Kafka", callback_data="to_shoop")
     )
-    bot.send_message(
-        message.chat.id,
-        "What would you like to do with $KAFKA?",
-        reply_markup=markup
-    )
+    bot.send_message(message.chat.id, "Choose:", reply_markup=markup)
 
-
-@bot.callback_query_handler(func=lambda call: call.data in ["to_tasks", "to_shoop"])
-def handle_inline_buttons(call):
+@bot.callback_query_handler(func=lambda c: c.data in ["to_tasks", "to_shoop"])
+def inline(call):
     if call.data == "to_tasks":
         handle_tasks(call.message)
-    elif call.data == "to_shoop":
+    else:
         handle_shoop(call.message)
-
     bot.answer_callback_query(call.id)
