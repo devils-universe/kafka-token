16d67aa7c1ee165dfa633045d22fecfeaa5e135b
devils-universe <jaba_juba420@mail.ru>
2025-07-28 17:19:03 +0700

    Update bot.py

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 from telebot import TeleBot
 
-load_dotenv()
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is not set")
