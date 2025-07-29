99d5a888336a3151fde1b7c3b80606e576ba98dc
devils-universe <jaba_juba420@mail.ru>
2025-07-29 11:45:58 +0700

    Update and rename stickers.py to web

--- TelegramBot/handlers/stickers.py
+++
@@ -1,5 +0,0 @@
-from bot import bot
-
-@bot.message_handler(func=lambda msg: msg.text == "🎨 Custom Sticker")
-def handle_sticker(message):
-    bot.send_message(message.chat.id, "🎨 Send your idea for a custom sticker.")
