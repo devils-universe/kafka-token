ed3e1487adf98cf20ad5063788205234ad2143c5
devils-universe <jaba_juba420@mail.ru>
2025-08-13 22:45:06 +0700

    Update airdrop.py

--- TelegramBot/handlers/airdrop.py
+++ TelegramBot/handlers/airdrop.py
@@ -4,6 +4,4 @@ from utils.language import t
 @bot.message_handler(func=lambda m: m.text in {"🎁 Airdrop", "🎁 Аирдроп"})
 def handle_airdrop(message):
     uid = message.from_user.id
-    bot.send_message(message.chat.id, f"{t(uid, 'opening_airdrop')}")
-    # Тут твоя логика аирдропа
     bot.send_message(message.chat.id, f"🎁 {t(uid, 'coming_soon')}")
