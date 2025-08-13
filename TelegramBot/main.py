781e2b5558f2aef15c23b079f85101ceb932f522
devils-universe <jaba_juba420@mail.ru>
2025-08-13 23:35:27 +0700

    Update main.py

--- TelegramBot/main.py
+++ TelegramBot/main.py
@@ -7,5 +7,8 @@ import handlers.airdrop
 import handlers.game
 import handlers.web
 
+from handlers.buy_ru import *      
+from handlers.base_ru_patch import *        
+
 if __name__ == "__main__":
     bot.polling(none_stop=True)
