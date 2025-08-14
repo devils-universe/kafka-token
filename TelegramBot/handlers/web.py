bd697b075d6e72dd25ec75b280421c97fde7a90d
devils-universe <jaba_juba420@mail.ru>
2025-08-14 09:13:23 +0700

    Update web.py

--- TelegramBot/handlers/web.py
+++ TelegramBot/handlers/web.py
@@ -2,4 +2,4 @@ from bot import bot
 
 @bot.message_handler(func=lambda msg: msg.text in {"🌀 Web", "🌀 Веб"})
 def handle_web(message):
-    bot.send_message(message.chat.id, "https://devilsuniverse.com/", disable_web_page_preview=True)
+    bot.send_message(message.chat.id, "https://devilsuniverse.com", disable_web_page_preview=True)
